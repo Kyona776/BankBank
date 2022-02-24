e3f7e73b9de05dcd8eee276c0838bf4c9be5f2c8
Kyona776 <kyona5641@gmail.com>
2022-02-25 03:12:40 +0800

    init push

---
+++ contracts/Dependencies/BaseUnits.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+
+contract BaseUnits {
+    uint internal constant DECIMAL_PRECISION = 1e18;
+    uint internal constant PRCT_PRECISION = 1e20;
+    uint internal constant num_decimals = 18;
+    uint internal constant prct_decimasl = 20;
+}
